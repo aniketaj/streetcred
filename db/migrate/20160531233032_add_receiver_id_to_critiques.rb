8551c8592c4ef2cca75a7625c6855195e92d5b1f
aniketaj <ajoglekar2017@kellogg.northwestern.edu>
2016-06-08 10:03:07 -0500

    16 After adding input to receiver ID in new feedback
    in this commit the receiver ID is not a hidden input (kept it as visible
    right now to see if it works)

--- db/migrate/20160531233032_add_receiver_id_to_critiques.rb
+++ db/migrate/20160531233032_add_receiver_id_to_critiques.rb
@@ -1,5 +1,5 @@
 class AddReceiverIdToCritiques < ActiveRecord::Migration
   def change
-    add_column :critiques, :receiver_id, :integer
+    add_column :critiques, :receiver_id, :string
   end
 end
